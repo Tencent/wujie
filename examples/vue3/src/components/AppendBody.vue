6281a6285047ba3a5ec2a14a06da419ceef6ddac
yiludege <xzd123@qq.com>
2022-06-30 20:53:00 +0800

    docs: 修改demo样式

--- examples/vue3/src/components/AppendBody.vue
+++ examples/vue3/src/components/AppendBody.vue
@@ -22,7 +22,7 @@ export default {
       let ele = document.createElement("div");
       ele.innerHTML = "body中插入position为fixed元素";
       ele.style.cssText =
-        "position: fixed; top: 0; left: 0; width: 400px; text-align: center; height: 100px; line-height: 100px; background-color: #41b883";
+        "position: fixed; z-index: 100; top: 0; left: 0; width: 400px; text-align: center; height: 100px; line-height: 100px; background-color: #41b883";
       document.body.appendChild(ele);
       this.dialog = ele;
     },
