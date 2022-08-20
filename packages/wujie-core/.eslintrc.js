f16941149c37f2e82bb8b884fc87393c76a4441f
ThinkHalo <1750367868@qq.com>
2022-08-20 11:09:56 +0800

    fix: wujie-core package has eslint error (#81)

--- packages/wujie-core/.eslintrc.js
+++ packages/wujie-core/.eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true,
   },
   extends: ["eslint:recommended", "plugin:prettier/recommended", "prettier"],
   parser: "@typescript-eslint/parser",
