93e61f916b4d7c7d6f413df8be227e79e58fa093
fanzehong <zehong.fan@shopee.com>
2022-08-30 21:08:04 +0800

    fix: 修复window被proxy,导致作用域错误

--- packages/wujie-core/src/utils.ts
+++ packages/wujie-core/src/utils.ts
@@ -73,10 +73,23 @@ export function isConstructable(fn: () => any | FunctionConstructor) {
   return constructable;
 }
 
+const setFnCacheMap = new WeakMap<CallableFunction, CallableFunction>();
+export function checkProxyFunction(value: any) {
+  if (isCallable(value) && !isBoundedFunction(value) && !isConstructable(value)) {
+    if (!setFnCacheMap.has(value)) {
+      setFnCacheMap.set(value, value);
+    }
+  }
+}
+
 export function getTargetValue(target: any, p: any): any {
   const value = target[p];
+  if (setFnCacheMap.has(value)) {
+    return setFnCacheMap.get(value);
+  }
   if (isCallable(value) && !isBoundedFunction(value) && !isConstructable(value)) {
     const boundValue = Function.prototype.bind.call(value, target);
+    setFnCacheMap.set(value, boundValue);
 
     for (const key in value) {
       boundValue[key] = value[key];
