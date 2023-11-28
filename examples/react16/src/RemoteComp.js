27f4d771dcce4f294b8d346c65c7c36be78d536d
fantasticsoul <zhongzhengkai@gmail.com>
2023-11-28 22:11:06 +0800

    feat: bump hel lib

--- examples/react16/src/RemoteComp.js
+++ examples/react16/src/RemoteComp.js
@@ -2,7 +2,14 @@ import React from "react";
 import { useRemoteComp } from 'hel-micro-react';
 
 export default function RemoteComp() {
-  const Comp = useRemoteComp("hel-tpl-remote-react-comps", "HelloRemoteReactComp");
+  const Comp = useRemoteComp(
+    // see https://github.com/hel-e co/hel-tpl-remote-react-comp
+    "hel-tpl-remote-react-comps",
+    // see https://github.com/hel-eco/hel-tpl-remote-react-comp-ts
+    // "hel-tpl-remote-react-comps-ts",
+    "HelloRemoteReactComp",
+    { versionId: "1.3.2" }
+  );
   return <div>
     <h1>这是一个懒加载的远程react组件</h1>
     <Comp />
--- examples/react16/src/index.js
+++ examples/react16/src/index.js
@@ -5,6 +5,6 @@ import { bindReactRuntime, preFetchLib } from 'hel-micro';
 bindReactRuntime({ React, ReactDOM });
 
 (async function () {
-  await preFetchLib('hel-lodash'); // 预加载 hel-lodash，方便其他地方可以静态导入 hel-lodash
+  await preFetchLib('hel-lodash','2.3.0'); // 预加载 hel-lodash，方便其他地方可以静态导入 hel-lodash
   await import('./loadApp');
 })().catch(console.error);
