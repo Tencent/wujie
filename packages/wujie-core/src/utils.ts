41a5309de9e39539d6684a424302bff2b7965db2
AttackXiaoJinJin <wwwcenjin1314@163.com>
2024-11-24 11:23:11 +0800

    fix: 修复多个子应用启动，拿到的全局对象缓存都是第一个子应用全局对象的bug (#792)

--- packages/wujie-core/src/utils.ts
+++ packages/wujie-core/src/utils.ts
@@ -79,24 +79,36 @@ export function isConstructable(fn: () => any | FunctionConstructor) {
   return constructable;
 }
 
-const setFnCacheMap = new WeakMap<CallableFunction, CallableFunction>();
-export function checkProxyFunction(value: any) {
+// 修复多个子应用启动，拿到的全局对象都是第一个子应用全局对象的bug：https://github.com/Tencent/wujie/issues/770
+const setFnCacheMap = new WeakMap<
+  Window | Document | ShadowRoot | Location,
+  WeakMap<CallableFunction, CallableFunction>
+>();
+
+export function checkProxyFunction(target: Window | Document | ShadowRoot | Location, value: any) {
   if (isCallable(value) && !isBoundedFunction(value) && !isConstructable(value)) {
-    if (!setFnCacheMap.has(value)) {
-      setFnCacheMap.set(value, value);
+    if (!setFnCacheMap.has(target)) {
+      setFnCacheMap.set(target, new WeakMap());
+      setFnCacheMap.get(target).set(value, value);
+    } else if (!setFnCacheMap.get(target).has(value)) {
+      setFnCacheMap.get(target).set(value, value);
     }
   }
 }
 
 export function getTargetValue(target: any, p: any): any {
   const value = target[p];
-  if (setFnCacheMap.has(value)) {
-    return setFnCacheMap.get(value);
+  if (setFnCacheMap.has(target) && setFnCacheMap.get(target).has(value)) {
+    return setFnCacheMap.get(target).get(value);
   }
   if (isCallable(value) && !isBoundedFunction(value) && !isConstructable(value)) {
     const boundValue = Function.prototype.bind.call(value, target);
-    setFnCacheMap.set(value, boundValue);
-
+    if (setFnCacheMap.has(target)) {
+      setFnCacheMap.get(target).set(value, boundValue);
+    } else {
+      setFnCacheMap.set(target, new WeakMap());
+      setFnCacheMap.get(target).set(value, boundValue);
+    }
     for (const key in value) {
       boundValue[key] = value[key];
     }
