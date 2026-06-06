fba3af273ad9a9b4818b0b67879f502aa1c3691d
yiludege <xzd123@qq.com>
2026-06-06 11:16:13 +0800

    test: 添加内联事件处理器测试页面
    - 在 vue3 示例项目中添加 InlineEvent.vue 测试页面
    - 测试场景包括：基本功能、多参数、全局变量访问、复杂表达式、事件对象、多事件
    - 添加相应的路由配置

--- examples/vue3/src/router/index.js
+++ examples/vue3/src/router/index.js
@@ -14,42 +14,32 @@ const routes = [
   {
     path: "/dialog",
     name: "Dialog",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ "../views/Dialog.vue"),
+    component: () => import("../views/Dialog.vue"),
   },
   {
     path: "/location",
     name: "Location",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ "../views/Location.vue"),
+    component: () => import("../views/Location.vue"),
   },
   {
     path: "/state",
     name: "State",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ "../views/State.vue"),
+    component: () => import("../views/State.vue"),
   },
   {
     path: "/contact",
     name: "Contact",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ "../views/Communication.vue"),
+    component: () => import("../views/Communication.vue"),
   },
   {
     path: "/postmessage",
     name: "Postmessage",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ "../views/PostMessage.vue"),
+    component: () => import("../views/PostMessage.vue"),
+  },
+  {
+    path: "/inline-event",
+    name: "InlineEvent",
+    component: () => import("../views/InlineEvent.vue"),
   },
 ];
 
